79ccde05eccbc664587da08e41717b03d8d26b7e
Kevin Stradtman <stradtkt22@gmail.com>
2019-12-29 18:35:18 -0500

    Added page banner function

--- functions.php
+++ functions.php
@@ -14,7 +14,7 @@ function university_features() {
   add_theme_support('post-thumbnails');
   add_image_size( 'professorLandscape', 400, 260, true );
   add_image_size( 'professorPortrait', 480, 650, true );
-  add_image_size( 'pageBanner' 1500, 350, true );
+  add_image_size( 'pageBanner', 1500, 350, true );
 }
 
 add_action('after_setup_theme', 'university_features');
@@ -43,3 +43,31 @@ function university_adjust_queries($query) {
 }
 
 add_action('pre_get_posts', 'university_adjust_queries');
+
+
+function pageBanner($args = NULL) {
+  if($args['title']) {
+    $args['title'] = get_the_title();
+  }
+  if($args['subtitle']) {
+    $args['subtitle'] = get_field('page_banner_subtitle');
+  }
+  if(!$args['photo']) {
+    if(get_field('page_banner_background_image')) {
+        $args['photo'] = get_field('page_banner_background_image')['sizes']['pageBanner'];
+    } else {
+      $args['photo'] = get_theme_file_uri( '/images/ocean.jpg' );
+    }
+  }
+  ?>
+   <div class="page-banner">
+      <div class="page-banner__bg-image" style="background-image: url(<?php $args['photo']; ?>);"></div>
+      <div class="page-banner__content container container--narrow">
+        <h1 class="page-banner__title"><?php $args['title']; ?></h1>
+        <div class="page-banner__intro">
+          <p><?php $args['subtitle']; ?></p>
+        </div>
+      </div>  
+    </div>
+  <?php 
+}
