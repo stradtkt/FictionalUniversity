500b59d0528fda8e34001a199d15d1a9f77fb592
Kevin Stradtman <stradtkt22@gmail.com>
2019-12-30 14:57:03 -0500

    Got search to work

--- footer.php
+++ footer.php
@@ -55,12 +55,16 @@
     <div class="search-overlay__top">
       <div class="container">
         <i class="fa fa-search search-overlay__icon" aria-hidden="true"></i>
-        <input type="text" id="search-term" placeholder="What are you looking for?" class="search-term">
+        <input type="text" class="search-term" placeholder="What are you looking for?" id="search-term">
         <i class="fa fa-window-close search-overlay__close" aria-hidden="true"></i>
       </div>
     </div>
-  </div>
+    
+    <div class="container">
+      <div id="search-overlay__results"></div>
+    </div>
 
+  </div>
 <?php wp_footer(); ?>
 </body>
 </html>
--- js/modules/Search.js
+++ js/modules/Search.js
@@ -1,35 +1,70 @@
 (function($) {
     class Search {
+        // 1. describe and create/initiate our object
         constructor() {
-            this.openButton = $('.js-search-trigger');
-            this.closeButton = $('.search-overlay__close');
-            this.searchOverlay = $(".search-overlay");
-            this.events();
-            this.isOverlayOpen = false;
+          this.resultsDiv = $("#search-overlay__results");
+          this.openButton = $(".js-search-trigger");
+          this.closeButton = $(".search-overlay__close");
+          this.searchOverlay = $(".search-overlay");
+          this.searchField = $("#search-term");
+          this.events();
+          this.isOverlayOpen = false;
+          this.isSpinnerVisible = false;
+          this.previousValue;
+          this.typingTimer;
         }
         events() {
-            this.openButton.on("click", this.openOverlay.bind(this));
-            this.closeButton.on("click", this.closeOverlay.bind(this));
-            $(document).on("keydown", this.keyPressDispatcher.bind(this));
+          this.openButton.on("click", this.openOverlay.bind(this));
+          this.closeButton.on("click", this.closeOverlay.bind(this));
+          $(document).on("keydown", this.keyPressDispatcher.bind(this));
+          this.searchField.on("keyup", this.typingLogic.bind(this));
         }
+        typingLogic() {
+          if (this.searchField.val() != this.previousValue) {
+            clearTimeout(this.typingTimer);
+            if (this.searchField.val()) {
+              if (!this.isSpinnerVisible) {
+                this.resultsDiv.html('<div class="spinner-loader"></div>');
+                this.isSpinnerVisible = true;
+              }
+              this.typingTimer = setTimeout(this.getResults.bind(this), 2000);
+            } else {
+              this.resultsDiv.html('');
+              this.isSpinnerVisible = false;
+            }
+          }
+          this.previousValue = this.searchField.val();
+        }
+      
+        getResults() {
+          this.resultsDiv.html("Imagine real search results here...");
+          this.isSpinnerVisible = false;
+        }
+      
         keyPressDispatcher(e) {
-            if(e.keyCode == 83 && !this.isOverlayOpen) {
-                this.openOverlay();
-            }
-            if(e.keyCode == 27 && this.isOverlayOpen) {
-                this.closeOverlay();
-            }
+          if (e.keyCode == 83 && !this.isOverlayOpen && !$("input, textarea").is(':focus')) {
+            this.openOverlay();
+          }
+      
+          if (e.keyCode == 27 && this.isOverlayOpen) {
+            this.closeOverlay();
+          }
+      
         }
+      
         openOverlay() {
-            this.searchOverlay.addClass("search-overlay--active");
-            $("body").addClass("body-no-scroll");
-            this.isOverlayOpen = true;
+          this.searchOverlay.addClass("search-overlay--active");
+          $("body").addClass("body-no-scroll");
+          console.log("our open method just ran!");
+          this.isOverlayOpen = true;
         }
+      
         closeOverlay() {
-            this.searchOverlay.removeClass("search-overlay--active");
-            $("body").removeClass("body-no-scroll");
-            this.isOverlayOpen = false;
+          this.searchOverlay.removeClass("search-overlay--active");
+          $("body").removeClass("body-no-scroll");
+          console.log("our close method just ran!");
+          this.isOverlayOpen = false;
         }
-    }
+      }
     let search = new Search();
 })(jQuery);
